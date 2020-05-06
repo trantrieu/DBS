ae216c3d840519bbd602723e734b7fc1fd539d17
Trieu tran <trantrieu89@gmail.com>
2020-05-06 16:36:45 +0800

    Add more espresso tests for detail screen, add test for edit screen

--- app/src/androidTest/java/com/dbs/detail/DetailActivityTest.kt
+++ app/src/androidTest/java/com/dbs/detail/DetailActivityTest.kt
@@ -1,11 +1,16 @@
 package com.dbs.detail
 
+import android.app.Activity.RESULT_OK
+import android.app.Instrumentation
 import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.toPackage
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import com.dbs.data.article.detail.Detail
@@ -48,6 +53,22 @@ internal class DetailActivityTest {
         Intents.intended(IntentMatchers.hasComponent(EditActivity::class.java.name))
     }
 
+    @Test
+    fun onActivityResult_receivedEditDetail_seesData() {
+        launchActivity()
+        mockEditDetail()
+        onView(withId(R.id.action_edit)).perform(click())
+        onView(withText("title1")).check(matches(isDisplayed()))
+        onView(withId(R.id.article_long_text)).check(matches(withText("edit text")))
+    }
+
+    private fun mockEditDetail() {
+        val resultData = Intent()
+        resultData.putExtra(DETAIL_EXTRA, createDetail(createArticle(), "edit text"))
+        val result = Instrumentation.ActivityResult(RESULT_OK, resultData)
+        intending(hasComponent("com.dbs.edit.EditActivity")).respondWith(result)
+    }
+
     private fun launchActivity() {
         val detail = createDetail(createArticle())
         val intent = Intent()
@@ -57,6 +78,6 @@ internal class DetailActivityTest {
 
     private fun createArticle() = Article(1, "title1", 1586404611, "short_desc1", "avatar_url1")
 
-    private fun createDetail(article: Article) = Detail(article.id, "long text", article)
+    private fun createDetail(article: Article, longText: String = "long text") = Detail(article.id, longText, article)
 
 }
---
+++ app/src/androidTest/java/com/dbs/edit/EditActivityTest.kt
@@ -0,0 +1,57 @@
+package com.dbs.edit
+
+import android.app.Instrumentation
+import android.content.Intent
+import androidx.core.app.TaskStackBuilder
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.dbs.R
+import com.dbs.data.article.detail.Detail
+import com.dbs.data.article.list.Article
+import com.dbs.detail.DetailActivity
+import com.dbs.detail.DetailActivity.Companion.DETAIL_EXTRA
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+internal class EditActivityTest {
+
+    @get:Rule
+    val activityTestRule = ActivityTestRule(EditActivity::class.java, false, false)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun clear() {
+        Intents.release()
+    }
+
+    @Test
+    fun onLoad_seesData() {
+        launchActivity()
+        onView(withText("title1")).check(matches(isDisplayed()))
+        onView(withId(R.id.edit_text_detail)).check(matches(withText("long text")))
+    }
+
+    private fun launchActivity() {
+        val detail = createDetail(createArticle())
+        val intent = Intent()
+        intent.putExtra(DETAIL_EXTRA, detail)
+        activityTestRule.launchActivity(intent)
+    }
+
+    private fun createArticle() = Article(1, "title1", 1586404611, "short_desc1", "avatar_url1")
+
+    private fun createDetail(article: Article) = Detail(article.id, "long text", article)
+}
